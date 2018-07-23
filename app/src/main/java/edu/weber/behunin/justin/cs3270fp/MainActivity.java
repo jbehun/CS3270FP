5060fd9f130deb1c20bbff97151987bf1b5e2e0c
Justin Behunin <>
2018-07-22 22:19:43 -0600

    Initial commit

---
+++ app/src/main/java/edu/weber/behunin/justin/cs3270fp/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.weber.behunin.justin.cs3270fp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
