d108299af5145ea38f1b799a4110cb6a011e4871
Justin Behunin <>
2018-07-25 23:01:32 -0600

    Created the sign in fragment and built the layout file.

--- app/src/main/java/edu/weber/behunin/justin/cs3270fp/MainActivity.java
+++ app/src/main/java/edu/weber/behunin/justin/cs3270fp/MainActivity.java
@@ -1,13 +1,28 @@
 package edu.weber.behunin.justin.cs3270fp;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        fm.beginTransaction().replace(R.id.frag1, new SignInFragement(), "FragSignIn")
+                .addToBackStack(null).commit();
     }
 }
