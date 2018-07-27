561c6021a0ac576c23991fc8460988a1501b05bb
Justin Behunin <>
2018-07-26 23:09:18 -0600

    Added functionality to sign in and create an account.

--- app/src/main/java/edu/weber/behunin/justin/cs3270fp/MainActivity.java
+++ app/src/main/java/edu/weber/behunin/justin/cs3270fp/MainActivity.java
@@ -3,23 +3,51 @@ package edu.weber.behunin.justin.cs3270fp;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
-
     @Override
-    protected void onPostResume() {
-        super.onPostResume();
+    protected void onResume() {
+        super.onResume();
 
+        //todo add sign out as a menu option once signed in
+        mAuth.signOut();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null) {
+            //if user not signed in goto sign fragment
+            userSignIn();
+
+        }else{
+            Toast toast = Toast.makeText(this, currentUser.getEmail(), Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+
+    private void userSignIn() {
         FragmentManager fm = getSupportFragmentManager();
 
         fm.beginTransaction().replace(R.id.frag1, new SignInFragement(), "FragSignIn")
--- app/src/main/java/edu/weber/behunin/justin/cs3270fp/SignInFragement.java
+++ app/src/main/java/edu/weber/behunin/justin/cs3270fp/SignInFragement.java
@@ -2,10 +2,21 @@ package edu.weber.behunin.justin.cs3270fp;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
@@ -13,6 +24,10 @@ import android.view.ViewGroup;
  */
 public class SignInFragement extends Fragment {
 
+    FirebaseAuth mAuth;
+    View root;
+    TextInputEditText txtEmail, txtPassword;
+    Button btnSignIn, btnCreate;
 
     public SignInFragement() {
         // Required empty public constructor
@@ -22,8 +37,100 @@ public class SignInFragement extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in_fragement, container, false);
+        return root = inflater.inflate(R.layout.fragment_sign_in_fragement, container, false);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        txtEmail = root.findViewById(R.id.inputEmail);
+        txtPassword = root.findViewById(R.id.inputPassword);
+        btnSignIn = root.findViewById(R.id.buttonSignIn);
+        btnCreate = root.findViewById(R.id.buttonCreate);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInUser();
+            }
+        });
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+
+    }
+
+    private void createUser() {
+
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        if (validInput()) {
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                //if succesfull go to plan fragment
+                                Log.d("test", "user successfully created");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Log.d("test", user.getEmail());
+                            } else {
+                                //if sign in fails, display a message to user
+                                Log.d("test", "Create user failed " + task.getException());
+                                Toast.makeText(getActivity(), "Creation Failed", Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        }
+                    });
+
+        } else {
+            Toast toast = Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    private void signInUser() {
+
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        if (validInput()) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                //Sign in successful go to plan fragment.
+                                Log.d("test", "Sign in successful");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(getActivity(), "Welcome " + user.getEmail(),
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                Log.d("test", "Sign in failed " + task.getException());
+                                Toast.makeText(getActivity(), "Unable to sign in",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        } else {
+            Toast toast = Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    private boolean validInput() {
+
+        //todo add input validation
+
+        return true;
+    }
 }
